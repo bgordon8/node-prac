d6539a69741382e1f633f8c1f5e5d406570c1166
bgordon8 <gordonbrian16@gmail.com>
2023-02-14 12:04:02 -0700

    rounds out async lesson

--- app.js
+++ app.js
@@ -15,13 +15,25 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
+
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("hello.txt", "Dummy");
-    // send redirect
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("hello.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<body><h1>Hello from node</h1></body>");
