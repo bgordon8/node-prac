583159c364171db86d932414f969d59460209c49
bgordon8 <gordonbrian16@gmail.com>
2023-02-14 11:44:32 -0700

    adds server req/res

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 //creating server
 
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("hello.txt", "Dummy");
+    // send redirect
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<body><h1>Hello from node</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
