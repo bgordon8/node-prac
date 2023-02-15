598ef32c8a6e5df52b5aa332ded57abdea667b7a
bgordon8 <gordonbrian16@gmail.com>
2023-02-15 09:32:30 -0700

    adds routes.js updates app.js

--- app.js
+++ app.js
@@ -1,44 +1,6 @@
-//creating server
-
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
+const routes = require("./routes");
 
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("hello.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  // here some practice
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<body><h1>Hello from node</h1></body>");
-  res.write("</html>");
-  res.end();
-});
-
+const server = http.createServer(routes);
 server.listen(3000);
