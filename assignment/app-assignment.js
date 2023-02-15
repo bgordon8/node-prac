91ecc9a7352bc04540c08d9c1afcbb83a656356e
bgordon8 <gordonbrian16@gmail.com>
2023-02-15 09:45:55 -0700

    adds assignment

---
+++ assignment/app-assignment.js
@@ -0,0 +1,6 @@
+// Spin up a node.js server on port 3000
+// Handle two routes '/' and '/users'
+//      return some greeting text on '/'
+//      return a list of dummy users (eg <ul><li>user 1</li></ul>)
+// Add a form with 'username' <input> to the '/' page and submit a POST request to '/create-server' upon a button click
+// Add the '/create-user' route and parse the incoming data(username) and log to console
