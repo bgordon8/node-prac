a4ef7c4b82466006d2ab030ca3f4524011f7d4ac
bgordon8 <gordonbrian16@gmail.com>
2023-03-08 15:28:57 -0700

    adds rootDir to PNF

--- routes/page-not-found.js
+++ routes/page-not-found.js
@@ -1,12 +1,11 @@
 const path = require("path");
 const express = require("express");
 
+const rootDir = require("../util/path");
 const router = express.Router();
 
 router.use("/", (req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "../", "views", "page-not-found.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "page-not-found.html"));
 });
 
 module.exports = router;
