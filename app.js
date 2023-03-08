25b4450e2357249ce8fce69d912df3be8908ec0e
bgordon8 <gordonbrian16@gmail.com>
2023-03-08 12:29:49 -0700

    seperates routes

--- app.js
+++ app.js
@@ -3,24 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoute = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use(express.urlencoded({ extended: true }));
-
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button>Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello from express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoute);
 
 app.listen(3000, () => {
   console.log("listening on port 3000...");
