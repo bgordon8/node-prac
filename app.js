09199779cdc7086e6daccc00d07aacb2e93b9188
bgordon8 <gordonbrian16@gmail.com>
2023-03-08 12:51:11 -0700

    updates route filter with admin

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const shopRoute = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoute);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000...");
--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/shop", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send("<h1>Shop</h1>");
 });
 
