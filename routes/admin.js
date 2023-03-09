457b7325be1321cb3598bc83ee0518daa9fef937
bgordon8 <gordonbrian16@gmail.com>
2023-03-09 13:56:47 -0700

    completes templating engines with ejs pug and hbs

--- routes/admin.js
+++ routes/admin.js
@@ -10,12 +10,19 @@ router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
   });
 });
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
+  products.push({
+    title: req.body.title,
+    price: req.body.price,
+    description: req.body.description,
+  });
   res.redirect("/");
 });
 
