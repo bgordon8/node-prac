9a4d04ec9fb7e5f121406e8db6f910259b305200
bgordon8 <gordonbrian16@gmail.com>
2023-03-08 18:37:24 -0700

    adds templeting engine pug ejs handlebars

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 const pageNotFoundRoute = require("./routes/page-not-found");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoute);
 app.use(pageNotFoundRoute);
 
